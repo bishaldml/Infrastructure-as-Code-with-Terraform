f97865b3ef0dc9a29f0d03849eca85e2a4cf68af
Bishal Dhimal <51133184+bishaldml@users.noreply.github.com>
2024-06-23 14:33:41 +0545

    Create main.tf

---
+++ 1. Infrastructure as Code with Terraform/main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "e2-medium"
+
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  allow_stopping_for_update = true 
+}
