0ca534513950a7570eae6eb56f748b3c394c94c0
Bishal Dhimal <51133184+bishaldml@users.noreply.github.com>
2024-06-23 14:32:28 +0545

    Update exp.tf

--- 2. Creating Resource Dependencies with Terraform/exp.tf
+++ 2. Creating Resource Dependencies with Terraform/exp.tf
@@ -1,4 +1,3 @@
-# Create a new instance that uses the bucket
 resource "google_compute_instance" "another_instance" {
 
   name         = "terraform-instance-2"
@@ -16,4 +15,12 @@ resource "google_compute_instance" "another_instance" {
   # Tells Terraform that this VM instance must be created only after the
   # storage bucket has been created.
   depends_on = [google_storage_bucket.example_bucket]
+}
+resource "google_storage_bucket" "example_bucket" {
+  name     = "<UNIQUE-BUCKET-NAME>"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
 }
